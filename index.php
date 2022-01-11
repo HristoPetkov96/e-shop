11c7d16fe37e1cb51faf0866b56acc389f6ce4da
hristo <hristopetkov96@gmail.com>
2022-01-11 22:11:42 +0200

    change

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <head>
 <title>Portfolio</title>
 <main>
-    <p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>
-    <p>Purr jump eat the grass rip the couch scratched sunbathe, shed everywhere rip the couch sleep in the sink fluffy fur catnip scratched.</p
+  
 </main>
 <body><h1>Calipsso</h1></body>
 </head>
